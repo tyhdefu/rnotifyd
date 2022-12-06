11e7f431563a648092327cca8d855425155f994d
James <jameshendry05@gmail.com>
2022-12-06 15:27:20 +0000

    Fix run log reading error

--- src/run_log.rs
+++ src/run_log.rs
@@ -3,7 +3,7 @@ use std::fs;
 use std::path::PathBuf;
 use rnotifydlib::config::JobDefinitionId;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct RunLog {
     last_run: HashMap<JobDefinitionId, u64>,
 }
@@ -22,8 +22,8 @@ impl RunLog {
         let mut map = HashMap::new();
 
         for line in s.lines()
-            .filter(|line| line.starts_with("#"))
-            .filter(|line| line.is_empty()) {
+            .filter(|line| !line.starts_with("#"))
+            .filter(|line| !line.is_empty()) {
 
             let mut split = line.split(":");
             let id_part = split.next().ok_or("Missing 1st part of colon seperated entry.".to_owned())?;
@@ -67,4 +67,23 @@ pub fn read_run_log(path: &PathBuf) -> RunLog {
 pub fn write_run_log(run_log: &RunLog, path: &PathBuf) -> std::io::Result<()> {
     let s = run_log.write_to_string();
     fs::write(path, s)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_read() {
+        let s = "hello-world:1670340125\nbeep-boop:1670370255";
+        let parsed = RunLog::read_from_string(s).unwrap();
+        let mut expected_map = HashMap::new();
+        expected_map.insert(JobDefinitionId::try_new("hello-world".into()).unwrap(), 1670340125);
+        expected_map.insert(JobDefinitionId::try_new("beep-boop".into()).unwrap(), 1670370255);
+        let expected = RunLog {
+            last_run: expected_map,
+        };
+
+        assert_eq!(expected, parsed);
+    }
 }
